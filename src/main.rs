ea20f187ab686511480f1d1b720aca65fba6cf3c
Ashwin Rohit Alagiri Rajan <marble@Ashwins-MacBook-Pro.local>
2022-12-25 15:14:26 +0300

    Add cmdline parsing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Opts {
-	#[clap(short, long)]
+	#[clap(short, long, help = "Path to input file")]
 	input: String,
-	#[clap(short, long)]
+	#[clap(short, long, help = "Path to stylesheet")]
 	stylesheet: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	convert_to_html("hello.wg", "style.css")?;
+	let opts: Opts = Opts::parse();
+	let input = std::fs::read_to_string(opts.input)?;
+	let stylesheet = std::fs::read_to_string(opts.stylesheet)?;
+	let html = convert_to_html(&input, &stylesheet)?;
 	Ok(())
 }
